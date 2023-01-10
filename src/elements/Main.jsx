6f25fd716bdf0b4a17a4ce926bd8259a14ebc301
BBSharps <billybriarsharps@gmail.com>
2023-01-10 12:50:16 +0000

    moved reviewState into Reviews

--- src/elements/Main.jsx
+++ src/elements/Main.jsx
@@ -2,37 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Reviews from "./Reviews";
 import ReviewById from "./review_by_id";
 import Home from "./Home";
-import { useState, useEffect } from "react";
-import { getReviews } from "../utility/axios-request";
 
 function Main() {
-  const [reviewState, setReviewState] = useState([]);
-  useEffect(() => {
-    getReviews().then((response) => {
-      setReviewState(response);
-    });
-  }, []);
   return (
     <main>
       <Routes>
-        <Route
-          path="/*"
-          element={
-            <Reviews
-              reviewState={reviewState}
-              setReviewState={setReviewState}
-            />
-          }
-        />
-        <Route
-          path="/reviews"
-          element={
-            <Reviews
-              reviewState={reviewState}
-              setReviewState={setReviewState}
-            />
-          }
-        />
+        <Route path="/*" element={<Reviews />} />
+        <Route path="/reviews" element={<Reviews />} />
         <Route path="/reviews/:id" element={<ReviewById />} />
       </Routes>
     </main>
--- src/elements/Reviews.jsx
+++ src/elements/Reviews.jsx
@@ -1,7 +1,15 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getReviews } from "../utility/axios-request";
 import ReviewCard from "./reviews_li_card";
 
-function Reviews({ reviewState, setReviewState }) {
+function Reviews() {
+  const [reviewState, setReviewState] = useState([]);
+  useEffect(() => {
+    getReviews().then((response) => {
+      setReviewState(response);
+    });
+  }, []);
   return (
     <div>
       <ul className="reviewsList">
